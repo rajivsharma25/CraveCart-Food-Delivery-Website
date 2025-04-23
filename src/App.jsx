b83eae8a699044f3a1ecf563253671e8c548d19d
rajivsharma25 <rajivsharma93056@gmail.com>
2025-04-24 04:45:16 +0530

    Production build

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/order' element={<PlaceOrder/>} />
+        <Route path='*' element={<Home/>} />
       </Routes>
     </div>
     <Footer/>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CraveCart-Food-Delivery-Website/',  // Add this line
+  base: '/CraveCart-Food-Delivery-Website/',  
   plugins: [react()],
 })
